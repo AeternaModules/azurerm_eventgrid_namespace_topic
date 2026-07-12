0c80c284ebbd64659ea61c4c430032e0c754288d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "eventgrid_namespace_topics_id" {
+  description = "Map of id values across all eventgrid_namespace_topics, keyed the same as var.eventgrid_namespace_topics"
+  value       = { for k, v in azurerm_eventgrid_namespace_topic.eventgrid_namespace_topics : k => v.id }
+}
 output "eventgrid_namespace_topics_event_retention_in_days" {
   description = "Map of event_retention_in_days values across all eventgrid_namespace_topics, keyed the same as var.eventgrid_namespace_topics"
   value       = { for k, v in azurerm_eventgrid_namespace_topic.eventgrid_namespace_topics : k => v.event_retention_in_days }
